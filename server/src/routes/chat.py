7b38379cfbb26f89b01f1a865281eec5116273cd
Abeeb Adesina <deolabeeb@gmail.com>
2024-08-21 18:36:16 +0100

    Done

--- server/src/routes/chat.py
+++ server/src/routes/chat.py
@@ -1,5 +1,6 @@
 import os
-from fastapi import APIRouter, FastAPI, WebSocket, Request
+from fastapi import APIRouter, FastAPI, WebSocket, Request, BackgroundTasks, HTTPException
+import uuid
 
 chat = APIRouter()
 
@@ -7,9 +8,20 @@ chat = APIRouter()
 # @desc    Route to generate chat token
 # @access  Public
 
+
 @chat.post("/token")
-async def token_generator(request: Request):
-    return None
+async def token_generator(name: str, request: Request):
+
+    if name == "":
+        raise HTTPException(status_code=400, detail={
+            "loc": "name",  "msg": "Enter a valid name"})
+
+    token = str(uuid.uuid4())
+
+    data = {"name": name, "token": token}
+
+    return data
+
 
 
 # @route   POST /refresh_token
