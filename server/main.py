fcaf3b53b84cc7bd0e414a38c6baf257e20e676c
Abeeb Adesina <deolabeeb@gmail.com>
2024-08-21 06:56:34 +0100

    Integrate chat.py

--- server/main.py
+++ server/main.py
@@ -2,14 +2,16 @@ from fastapi import FastAPI, Request
 import uvicorn
 import os
 from dotenv import load_dotenv
+from src.routes.chat import chat  # Import the chat router
 
 load_dotenv()
 
 api = FastAPI()
+api.include_router(chat)  # Include the chat router
 
 @api.get("/test")
 async def root():
-    return {"msg": "Adeola's API is Online"}
+    return {"msg": "Abu Umar's API is Online"}
 
 if __name__ == "__main__":
     if os.environ.get('APP_ENV') == "development":
